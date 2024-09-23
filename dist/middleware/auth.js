add77d8ba805e486cf4c4ae8925526971079b2b8
MariaGargoles <mariagargoles@live.com>
2024-09-23 12:52:31 +0200

    FIX: login

--- dist/middleware/auth.js
+++ dist/middleware/auth.js
@@ -3,11 +3,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.secretKey = void 0;
 exports.authTokenMiddleware = authTokenMiddleware;
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const error_1 = require("../utils/error");
-exports.secretKey = process.env.TOKEN_SECRET || 'G7f#4jK@9mL2xP!v';
 function authTokenMiddleware(req, res, next) {
     var _a;
     const token = (_a = req.header('Authorization')) === null || _a === void 0 ? void 0 : _a.split(' ')[1];
@@ -16,11 +14,10 @@ function authTokenMiddleware(req, res, next) {
         return;
     }
     try {
-        const decoded = jsonwebtoken_1.default.verify(token, exports.secretKey);
-        req.user = decoded;
+        jsonwebtoken_1.default.verify(token, process.env.TOKEN_SECRET);
         next();
     }
     catch (error) {
-        next(new error_1.ErrorApi('Invalid token', 403));
+        next(error_1.ErrorApi);
     }
 }
--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -2,23 +2,16 @@ import { Request, Response, NextFunction } from "express";
 import jwt from 'jsonwebtoken';
 import { ErrorApi } from "../utils/error";
 
-export const secretKey = process.env.TOKEN_SECRET || 'G7f#4jK@9mL2xP!v';
-
-interface CustomRequest extends Request {
-    user?: string | jwt.JwtPayload;
-}
-
-export function authTokenMiddleware(req: CustomRequest, res: Response, next: NextFunction): void {
+export function authTokenMiddleware(req: Request, res: Response, next: NextFunction): void {
     const token = req.header('Authorization')?.split(' ')[1];
     if (!token) {
         res.sendStatus(401);
         return;
     }
     try {
-        const decoded = jwt.verify(token, secretKey);
-        req.user = decoded;
-        next();
-    } catch (error) {
-        next(new ErrorApi('Invalid token', 403));
+        jwt.verify(token, process.env.TOKEN_SECRET!);
+            next();
+    } catch(error) {
+        next(ErrorApi);
     }
 }
